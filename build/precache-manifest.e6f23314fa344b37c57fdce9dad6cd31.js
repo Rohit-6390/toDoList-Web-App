bed41437b383f8f1b6c81935a76a86e32fbf6c51
rohitbadekar639 <rohitbadekar3@gmail.com>
2021-02-28 17:06:56 +0530

    My First Commit

---
+++ build/precache-manifest.e6f23314fa344b37c57fdce9dad6cd31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bf036aa4bbcfc2f6070bc4bde39b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c8d0cdc8986b11c02a",
+    "url": "/static/js/2.0ea2811c.chunk.js"
+  },
+  {
+    "revision": "3f117a2aaacd8749d2ee",
+    "url": "/static/js/main.15f0b353.chunk.js"
+  },
+  {
+    "revision": "bb603cdcecd78fe2119f",
+    "url": "/static/js/runtime-main.2a02be55.js"
+  }
+]);
